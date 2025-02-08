13a7d0b7f85eff8d27018d177f86056af9c61b7e
amaliekea <amje0001@stud.kea.dk>
2025-02-08 13:41:38 +0100

    init

--- src/main/java/org/example/unicode/model/Unicode.java
+++ src/main/java/org/example/unicode/model/Unicode.java
@@ -1,13 +1,25 @@
 package org.example.unicode.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Unicode {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
     int unicode;
     char bogstav;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public int getUnicode() {
         return unicode;
